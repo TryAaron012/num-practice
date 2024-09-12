dd624184105a8e5a66a5a545f0703328947f4536
Aaron Raymund Paguirigan <paguiriganaaronraymund@gmail.com>
2024-09-12 10:07:32 +0800

    third commit

---
+++ num.js
@@ -0,0 +1,29 @@
+const favnumber1 = 15;
+const favnumber2 = 7;
+const sum = favnumber1 + favnumber2;
+console.log ('Sum:', sum);
+
+const Favnumber1 = 15;
+const Favnumber2 = 7;
+const difference = Favnumber1 - Favnumber2;
+console.log ('Difference:', difference); 
+
+const favNumber1 = 15;
+const favNumber2 = 7;
+const product = favNumber1 * favNumber2;
+console.log ('Product:', product);
+
+const favnuMber1 = 15;
+const favnuMber2 = 7;
+const quotient = favnuMber1 * favnuMber2;
+console.log ('Quotient:', quotient);
+
+const favnumBer1 = 15;
+const favnumBer2 = 7;
+const remainder = favnumBer1 % favnumBer2;
+console.log ('Remainder:', remainder);
+
+const favnumbeR1 = 15;
+const favnumbeR2 = 7;
+const power = favnumbeR1 ** favnumbeR2;
+console.log ('Power:', power);
